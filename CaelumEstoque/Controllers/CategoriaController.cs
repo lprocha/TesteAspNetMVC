a33379d98a72ce7517f6d1d2fbe298232eb592b9
leand <leand@DESKTOP-AF92DAM>
2016-06-07 23:08:03 -0400

    Controller categoria

---
+++ CaelumEstoque/Controllers/CategoriaController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+
+namespace CaelumEstoque.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            CategoriasDAO dao = new CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = dao.Lista();
+            
+            return View(categorias);
+        }
+    }
+}
